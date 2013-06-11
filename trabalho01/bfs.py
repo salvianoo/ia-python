2e2fbfdd708abba1fce5356b50bd576532780b1a
Salviano Ludgerio <salviano.lfg@gmail.com>
2013-06-11 00:04:10 -0300

    Rename and remove files

--- trabalho01/bfs.py
+++ trabalho01/bfs.py
@@ -4,25 +4,25 @@ class BFS(object):
         self.start = start
         self.visited = []
 
-    def next_node(self):
-        return self.visited.pop(0)
-
-    def node_not_explored(self, node):
-        return node not in self.visited
-
-    def queue(self, node):
-        self.visited.append(node)
-
-    def neighbours(self, node):
-        return self.graph[node]
-
     def search(self, target):
-        self.queue(self.start)
+        self._queue(self.start)
         while self.visited:
-            node_explored = self.next_node()
+            node_explored = self._next_node()
             if node_explored == target:
                 return node_explored
-            for node in self.neighbours(node_explored):
-                if self.node_not_explored:
-                    self.queue(node)
+            for node in self._neighbours(node_explored):
+                if self._node_not_explored:
+                    self._queue(node)
         return '%s Not Found' % target
+
+    def _next_node(self):
+        return self.visited.pop(0)
+
+    def _node_not_explored(self, node):
+        return node not in self.visited
+
+    def _queue(self, node):
+        self.visited.append(node)
+
+    def _neighbours(self, node):
+        return self.graph[node]
---
+++ trabalho01/test_bfs.py
@@ -0,0 +1,5 @@
+from bfs import BFS
+from romenia_map import ROMENIA
+
+bfs = BFS(ROMENIA, start='Arad')
+print (bfs.search('Bucharest') == 'Bucharest')
